1678557756512c336f4daa4662abab0d0e767a7b
Thiago <thiagoandrecardoso@icloud.com>
2023-01-13 21:36:13 -0300

    Criado teste para SRP

--- src/main/java/SRP/bad_example/Office.java
+++ src/main/java/SRP/bad_example/Office.java
@@ -2,10 +2,10 @@ package SRP.bad_example;
 
 import SRP.good_example.CalcRule;
 import SRP.good_example.FifteenOrTwentyPercent;
-import SRP.good_example.TeenOrTwentyPercent;
+import SRP.good_example.TenOrTwentyPercent;
 
 public enum Office {
-    DEV(new TeenOrTwentyPercent()),
+    DEV(new TenOrTwentyPercent()),
     DBA(new FifteenOrTwentyPercent()),
     TESTER(new FifteenOrTwentyPercent());
 
